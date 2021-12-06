defb9bb9dcff58cf41414a32fe31b5ed56631488
raihaninfo <raihanmahmudi35@gmail.com>
2021-12-07 02:27:25 +0600

    format code

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ func main() {
 	var historyArr []string
 	isHistory := false
 
+	// history button
 	historyBtn := widget.NewButton("History", func() {
 		if isHistory {
 			historyOutput = ""
@@ -37,6 +38,8 @@ func main() {
 		isHistory = !isHistory
 		historyLabel.SetText(historyOutput)
 	})
+
+	// back button
 	backBtn := widget.NewButton("Back", func() {
 		if len(output) > 0 {
 			output = output[:len(output)-1]
@@ -44,81 +47,101 @@ func main() {
 		}
 
 	})
+
+	// clear button
 	claerBtn := widget.NewButton("clear", func() {
 		output = ""
 		input.SetText(output)
 	})
+
+	// open button
 	openBtn := widget.NewButton("(", func() {
 		output = output + "("
 		input.SetText(output)
 	})
+
+	//close button
 	closeBtn := widget.NewButton(")", func() {
 		output = output + ")"
 		input.SetText(output)
 	})
+
 	divisionBtn := widget.NewButton("/", func() {
 		output = output + "/"
 		input.SetText(output)
 	})
+
 	sevenBtn := widget.NewButton("7", func() {
 		output = output + "7"
 		input.SetText(output)
 	})
+
 	eightBtn := widget.NewButton("8", func() {
 		output = output + "8"
 		input.SetText(output)
 	})
+
 	nineBtn := widget.NewButton("9", func() {
 		output = output + "9"
 		input.SetText(output)
 	})
+
 	multiplyBtn := widget.NewButton("*", func() {
 		output = output + "*"
 		input.SetText(output)
 	})
+
 	foreBtn := widget.NewButton("4", func() {
 		output = output + "4"
 		input.SetText(output)
 	})
+
 	fiveBtn := widget.NewButton("5", func() {
 		output = output + "5"
 		input.SetText(output)
 	})
+
 	sixBtn := widget.NewButton("6", func() {
 		output = output + "6"
 		input.SetText(output)
 	})
+
 	minusBtn := widget.NewButton("-", func() {
 		output = output + "-"
 		input.SetText(output)
 	})
+
 	oneBtn := widget.NewButton("1", func() {
 		output = output + "1"
 		input.SetText(output)
 	})
+
 	twoBtn := widget.NewButton("2", func() {
 		output = output + "2"
 		input.SetText(output)
 	})
+
 	threeBtn := widget.NewButton("3", func() {
 		output = output + "3"
 		input.SetText(output)
 	})
+
 	plusBtn := widget.NewButton("+", func() {
 		output = output + "+"
 		input.SetText(output)
 	})
+
 	zeroBtn := widget.NewButton("0", func() {
 		output = output + "0"
 		input.SetText(output)
 	})
+
 	dotBtn := widget.NewButton(".", func() {
 		output = output + "."
 		input.SetText(output)
 	})
 
 	equalBtn := widget.NewButton("=", func() {
-
 		expression, err := govaluate.NewEvaluableExpression(output)
 		if err == nil {
 			result, err := expression.Evaluate(nil)
@@ -127,10 +150,8 @@ func main() {
 				strToAppend := output + " + " + ans
 				historyArr = append(historyArr, strToAppend)
 				output = ans
-
 			} else {
 				output = "error"
-
 			}
 		} else {
 			output = "error"
