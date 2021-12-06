1db87486a05fe1362d42a6ff5dbde6748e9a430b
raihaninfo <raihanmahmudi35@gmail.com>
2021-12-06 11:02:50 +0600

    create all number button function

--- main.go
+++ main.go
@@ -12,69 +12,97 @@ func main() {
 	w := a.NewWindow("Fyne Calculator")
 	w.Resize(fyne.NewSize(300, 400))
 
+	output := ""
+	input := widget.NewLabel(output)
+
 	historyBtn := widget.NewButton("History", func() {
 
 	})
 	backBtn := widget.NewButton("Back", func() {
+		if len(output) > 0 {
+			output = output[:len(output)-1]
+			input.SetText(output)
+		}
 
 	})
 	claerBtn := widget.NewButton("clear", func() {
-
+		output = ""
+		input.SetText(output)
 	})
-	openBtn := widget.NewButton("(", func() {})
-	closeBtn := widget.NewButton(")", func() {})
+	openBtn := widget.NewButton("(", func() {
+		output = output + "("
+		input.SetText(output)
+	})
+	closeBtn := widget.NewButton(")", func() {
+		output = output + ")"
+		input.SetText(output)
+	})
 	divisionBtn := widget.NewButton("/", func() {
-
+		output = output + "/"
+		input.SetText(output)
 	})
 	sevenBtn := widget.NewButton("7", func() {
-
+		output = output + "7"
+		input.SetText(output)
 	})
 	eightBtn := widget.NewButton("8", func() {
-
+		output = output + "8"
+		input.SetText(output)
 	})
 	nineBtn := widget.NewButton("9", func() {
-
+		output = output + "9"
+		input.SetText(output)
 	})
 	multiplyBtn := widget.NewButton("*", func() {
-
+		output = output + "*"
+		input.SetText(output)
 	})
 	foreBtn := widget.NewButton("4", func() {
-
+		output = output + "4"
+		input.SetText(output)
 	})
 	fiveBtn := widget.NewButton("5", func() {
-
+		output = output + "5"
+		input.SetText(output)
 	})
 	sixBtn := widget.NewButton("6", func() {
-
+		output = output + "6"
+		input.SetText(output)
 	})
 	minusBtn := widget.NewButton("-", func() {
-
+		output = output + "-"
+		input.SetText(output)
 	})
 	oneBtn := widget.NewButton("1", func() {
-
+		output = output + "1"
+		input.SetText(output)
 	})
 	twoBtn := widget.NewButton("2", func() {
-
+		output = output + "2"
+		input.SetText(output)
 	})
 	threeBtn := widget.NewButton("3", func() {
-
+		output = output + "3"
+		input.SetText(output)
 	})
 	plusBtn := widget.NewButton("+", func() {
-
+		output = output + "+"
+		input.SetText(output)
 	})
 	zeroBtn := widget.NewButton("0", func() {
-
+		output = output + "0"
+		input.SetText(output)
 	})
 	dotBtn := widget.NewButton(".", func() {
-
+		output = output + "."
+		input.SetText(output)
 	})
 	equalBtn := widget.NewButton("=", func() {
 
 	})
 
-	hello := widget.NewLabel("Hello Fyne!")
 	w.SetContent(container.NewVBox(
-		hello,
+		input,
 		container.NewGridWithColumns(1,
 			container.NewGridWithColumns(2,
 				historyBtn,
