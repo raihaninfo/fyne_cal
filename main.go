c31eb453899c358086107c503dae6c9143fc04d7
raihaninfo <raihanmahmudi35@gmail.com>
2021-12-06 07:47:21 +0600

    create basic app leyout

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
 func main() {
-	app.new()
+	a := app.New()
+	w := a.NewWindow("Fyne_cal")
+	w.Resize(fyne.NewSize(300, 400))
+
+	w.ShowAndRun()
+
 }
